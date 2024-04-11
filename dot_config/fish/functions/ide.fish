12b4a64e5a52c3cce8651bfba59fd5473cdfdb34
Remo Senekowitsch <remo@buenzli.dev>
2024-04-11 22:12:38 +0200

    zellij: improve ide script
    Adds a fuzzy picker for git repositories and automatically attaches
    to running sessions for those repositories.

--- dot_config/fish/functions/ide.fish
+++ dot_config/fish/functions/ide.fish
@@ -1,3 +1,28 @@
 function ide
-    zellij --layout ide
+    if set --query argv[1]
+        set repo $argv[1]
+    else
+        set repo (ls --oneline ~/repos | fzf)
+    end
+    if test "$repo" = ""
+        return
+    end
+    if contains "$repo" (ls ~/repos)
+        if zellij list-sessions --short | rg --quiet --fixed-strings -- "$repo"
+            zellij attach "$repo"
+        else
+            pushd ~/repos/$repo
+            zellij --layout ide --session $repo
+            popd
+        end
+    else if test -d $repo
+        pushd $repo
+        zellij --layout ide
+        popd
+    else
+        set_color --bold brred
+        echo -n "ERROR "
+        set_color normal
+        echo "'$repo' not found"
+    end
 end
