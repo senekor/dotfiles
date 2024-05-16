f0e67eef83579895b3483abda8194f016ca0c9ae
Remo Senekowitsch <remo@buenzli.dev>
2024-05-16 18:26:37 +0200

    fish: dynamically map f to jj or git by cwd

--- dot_config/fish/conf.d/abbrevs.fish
+++ dot_config/fish/conf.d/abbrevs.fish
@@ -1,10 +1,32 @@
 if status is-interactive
-    abbr --add -- t git
-    abbr --add -- f git # for US qwerty keyboards
-
     abbr --add -- j just
 
     abbr --add -- c cargo
 
     abbr --add -- lt eza --tree --level 2
+
+    # facilitate hybrid usage of git and jj
+    function gg --on-variable PWD
+        set --local current_dir (pwd)
+        while true
+            if [ -d "$current_dir/.jj" ]
+                abbr --add -- t jj
+                abbr --add -- f jj # for US qwerty keyboards
+                break
+            end
+            if [ -d "$current_dir/.git" ]
+                abbr --add -- t git
+                abbr --add -- f git # for US qwerty keyboards
+                break
+            end
+            if [ $current_dir = / ]
+                abbr --add -- t jj
+                abbr --add -- f jj # for US qwerty keyboards
+                break
+            end
+            set current_dir (path dirname $current_dir)
+        end
+    end
+
+    gg
 end
