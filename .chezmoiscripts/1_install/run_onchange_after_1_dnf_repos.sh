e7f4b68593cd3d8f027b3555a1dcb9305148b242
Remo Senekowitsch <remo@buenzli.dev>
2024-05-20 01:33:40 +0200

    vscodium: migrate from vscode

--- .chezmoiscripts/1_install/run_onchange_after_1_dnf_repos.sh
+++ .chezmoiscripts/1_install/run_onchange_after_1_dnf_repos.sh
@@ -24,9 +24,18 @@ if ! dnf repolist | grep docker &> /dev/null ; then
     sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
 fi
 
-if ! dnf repolist | grep code &> /dev/null ; then
-    sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-    sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+if ! dnf repolist | grep "com_paulcarroty_vscodium_repo" &> /dev/null ; then
+    sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
+    printf "
+[gitlab.com_paulcarroty_vscodium_repo]
+name=download.vscodium.com
+baseurl=https://download.vscodium.com/rpms/
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
+metadata_expire=1h
+" | sudo tee -a /etc/yum.repos.d/vscodium.repo > /dev/null
 fi
 
 copr_repos=(
