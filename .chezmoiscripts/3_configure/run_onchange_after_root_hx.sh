5dfc4889e14ce472ed8036a0111624b170f7e5f4
Remo Senekowitsch <remo@buenzli.dev>
2024-06-17 14:26:50 +0200

    fix root hx script
    `sd --preview` now emits colored output, which would put the ansi escape
    codes into the config file (making it invalid.)

--- .chezmoiscripts/3_configure/run_onchange_after_root_hx.sh
+++ .chezmoiscripts/3_configure/run_onchange_after_root_hx.sh
@@ -6,5 +6,5 @@ set -eo pipefail
 # helix config hash: {{ include "dot_config/helix/config.toml" | sha256sum }}
 
 sudo mkdir -p "/root/.config/helix"
-sd --preview '^theme.*$' 'theme = "zenburn"' "$HOME/.config/helix/config.toml" \
-    | sudo tee "/root/.config/helix/config.toml" > /dev/null
+sudo cp "$HOME/.config/helix/config.toml" "/root/.config/helix/config.toml"
+sudo sd '^theme.*$' 'theme = "zenburn"' "/root/.config/helix/config.toml"
