027320c374aa45de5a5a47ea5765c7e9fa93843b
Remo Senekowitsch <remo@buenzli.dev>
2024-08-15 19:17:19 +0200

    zellij: add charging indicator

--- dot_local/bin/executable_batstat.fish
+++ dot_local/bin/executable_batstat.fish
@@ -1,5 +1,9 @@
 #!/usr/bin/env fish
 
+if [ "$(cat /sys/class/power_supply/BAT?/status)" = Charging ]
+    set bolt "⚡"
+end
+
 set charge_now (cat /sys/class/power_supply/BAT?/charge_now)
 set charge_full (cat /sys/class/power_supply/BAT?/charge_full)
 
@@ -9,4 +13,4 @@ end
 
 set percentage (math "$charge_now * 100 / $charge_full" | cut --delimiter '.' --fields 1)
 
-echo "$percentage%"
+echo "$bolt$percentage%"
