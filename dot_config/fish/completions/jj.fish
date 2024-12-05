5b8c2930e8434b365cd9fe20e8246c8ada6e1761
Remo Senekowitsch <remo@buenzli.dev>
2024-12-05 22:42:24 +0100

    jj: add custom completions for jj clone

--- dot_config/fish/completions/jj.fish
+++ dot_config/fish/completions/jj.fish
@@ -1,1 +1,45 @@
 COMPLETE=fish jj | source
+
+# custom `jj clone` completions
+complete \
+    --command jj \
+    --condition "__fish_seen_subcommand_from clone" \
+    --arguments "(__jj_clone (commandline --current-token))"
+function __jj_clone --argument-names token
+    if string match "git@*" $token &>/dev/null
+        __jj_clone_from_host (string sub --start 5 $token)
+        return
+    end
+    echo "git@"
+end
+function __jj_clone_from_host --argument-names token
+    set --local hosts
+    for host in ~/repos/*.* # at least one dot for domains
+        set --append hosts (basename $host)
+    end
+    for host in $hosts
+        set --local host (basename $host)
+        if string match "$host*" $token &>/dev/null
+            set --local offset (math "$(string length $host) - 1")
+            set --local token (string sub --start $offset $token)
+            __jj_clone_from_owner $host $token
+            return
+        end
+    end
+    printf "git@%s:\n" $hosts
+end
+function __jj_clone_from_owner --argument-names host token
+    set --local owners
+    for owner in ~/repos/$host/*/
+        set --append owners (basename $owner)
+    end
+    for owner in $owners
+        if string match "$owner*" $token &>/dev/null
+            # set --local offset (math "$(string length $owner) - 1")
+            # set --local token (string sub --start $offset $token)
+            # __jj_clone_from_owner $host $token
+            return
+        end
+    end
+    printf "git@$host:%s/\n" $owners
+end
