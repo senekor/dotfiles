e634236a36d8d8b0646c38b8e3883e13c912c5d7
Remo Senekowitsch <remo@buenzli.dev>
2025-02-13 23:36:46 +0100

    bring back dynamic VCS alias

--- dot_config/fish/conf.d/abbrevs.fish
+++ dot_config/fish/conf.d/abbrevs.fish
@@ -1,6 +1,17 @@
 if status is-interactive
     abbr --add -- lt eza --tree --level 2
 
-    abbr --add -- t jj
-    abbr --add -- f jj # for US qwerty keyboards
+    # facilitate hybrid usage of git and jj
+    function gg --on-variable PWD
+        if [ -d "$PWD/.git" ]
+            abbr --add -- t git
+            abbr --add -- f git # for US qwerty keyboards
+        end
+        if [ -d "$PWD/.jj" ]
+            abbr --add -- t jj
+            abbr --add -- f jj # for US qwerty keyboards
+        end
+    end
+
+    gg
 end
