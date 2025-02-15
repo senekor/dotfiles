397741332f7ecb38b21d632e893c9fc51a5d0020
Remo Senekowitsch <remo@buenzli.dev>
2025-02-15 17:08:28 +0100

    fix fish abbreviation for vcs fr

--- dot_config/fish/conf.d/abbrevs.fish
+++ dot_config/fish/conf.d/abbrevs.fish
@@ -3,12 +3,24 @@ if status is-interactive
 
     # facilitate hybrid usage of git and jj
     function gg --on-variable PWD
-        if ! test -d "$PWD/.jj"
-            abbr --add -- t git
-            abbr --add -- f git # for US qwerty keyboards
-        else
-            abbr --add -- t jj
-            abbr --add -- f jj # for US qwerty keyboards
+        set --local current_dir (pwd)
+        while true
+            if [ -d "$current_dir/.jj" ]
+                abbr --add -- t jj
+                abbr --add -- f jj # for US qwerty keyboards
+                break
+            end
+            if [ -d "$current_dir/.git" ]
+                abbr --add -- t git
+                abbr --add -- f git # for US qwerty keyboards
+                break
+            end
+            if [ $current_dir = / ]
+                abbr --add -- t jj
+                abbr --add -- f jj # for US qwerty keyboards
+                break
+            end
+            set current_dir (path dirname $current_dir)
         end
     end
 
