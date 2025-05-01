539bee3ed7fc4ae78e27b1c59e3af331464544f7
Remo Senekowitsch <remo@buenzli.dev>
2025-05-01 16:45:26 +0200

    niri: add gaps and focus-ring to gui mode

--- dot_local/bin/executable_toggle_gui_mode.sh
+++ dot_local/bin/executable_toggle_gui_mode.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# TODO change niri config to be more space-wasteful and fancy in bloated mode
-
-if systemctl --user is-active waybar.service > /dev/null ; then
+if systemctl --user is-active waybar.service > /dev/null
+then
     systemctl --user stop waybar.service
+
+    sd 'gaps \d+' 'gaps 0' ~/.config/niri/config.kdl
+    sd '// off // focus-ring' 'off // focus-ring' ~/.config/niri/config.kdl
+    sd 'window-rule \{ // rounded corners' '/-window-rule { // rounded corners' ~/.config/niri/config.kdl
+
     exit 0
 fi
 
 systemctl --user start waybar.service
+
+sd 'gaps \d+' 'gaps 16' ~/.config/niri/config.kdl
+sd 'off // focus-ring' '// off // focus-ring' ~/.config/niri/config.kdl
+sd '/-window-rule \{ // rounded corners' 'window-rule { // rounded corners' ~/.config/niri/config.kdl
