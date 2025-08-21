c6b7e9e3a7e051b24a77c790b15707b4344b0860
Remo Senekowitsch <remo@buenzli.dev>
2025-08-21 16:40:56 +0200

    fix setting of SSH_AUTH_SOCK variable
    Firstly, testing if it exists doesn't work (anymore?), because there is
    some "keyring" agent running already. Secondly, I had to add the new
    tests in the git cloning script as well, because when that is run at
    first init time, the fish config is not loaded into the environment yet.

--- dot_config/fish/conf.d/ssh_agent.fish
+++ dot_config/fish/conf.d/ssh_agent.fish
@@ -1,6 +1,6 @@
 # When we're ssh'ed onto a server, the SSH_AUTH_SOCK variable is already set
 # by the agent forwarding. In that case, we shouldn't overwrite it, because
 # 1Password is not running on servers.
-if test -z "$SSH_AUTH_SOCK"
+if test -e ~/.1password/agent.sock
     set --global --export SSH_AUTH_SOCK ~/.1password/agent.sock
 end
