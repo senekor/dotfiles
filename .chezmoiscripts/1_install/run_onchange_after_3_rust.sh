bae11204a9f6ed93752258e2542a5d9137747041
Remo Senekowitsch <remo@buenzli.dev>
2025-08-24 08:14:34 +0200

    rust: install nightly toolchain

--- .chezmoiscripts/1_install/run_onchange_after_3_rust.sh
+++ .chezmoiscripts/1_install/run_onchange_after_3_rust.sh
@@ -4,3 +4,8 @@ set -eo pipefail
 if ! which rustup &> /dev/null ; then
     rustup-init -y --no-modify-path --component rust-analyzer
 fi
+
+# {{ template "skipOnServer" . }}
+
+# for nightly formatting in jj-vcs/jj
+rustup toolchain add nightly
