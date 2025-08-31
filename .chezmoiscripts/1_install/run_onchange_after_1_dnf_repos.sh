f0b78591bfa58f60ad17746675e683c74a6aaa21
Remo Senekowitsch <remo@buenzli.dev>
2025-08-31 13:10:38 +0200

    codium: switch back to rpm package
    too much annoying isolation with flatpak

--- .chezmoiscripts/1_install/run_onchange_after_1_dnf_repos.sh
+++ .chezmoiscripts/1_install/run_onchange_after_1_dnf_repos.sh
@@ -16,12 +16,26 @@ if ! dnf repo list | grep fedora-cisco-openh264 &> /dev/null ; then
     sudo dnf config-manager --set-enabled fedora-cisco-openh264
 fi
 
+if ! dnf repo list | grep "com_paulcarroty_vscodium_repo" &> /dev/null ; then
+    # The vscodium flatpak is kinda bad / applies too much isolation.
+    sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
+    printf "
+[gitlab.com_paulcarroty_vscodium_repo]
+name=download.vscodium.com
+baseurl=https://download.vscodium.com/rpms/
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
+metadata_expire=1h
+" | sudo tee -a /etc/yum.repos.d/vscodium.repo > /dev/null
+fi
+
 repos_to_remove=(
     # preinstalled repos I don't want
     _copr:copr.fedorainfracloud.org:phracek:PyCharm.repo
     google-chrome.repo
     # repos I installed myself but don't want / need anymore:
-    vscodium.repo # replaced with flatpak
     _copr:copr.fedorainfracloud.org:atim:starship.repo # via cargo-binstall
     _copr:copr.fedorainfracloud.org:claaj:typst.repo # via mise per repo
     _copr:copr.fedorainfracloud.org:varlad:zellij.repo # via cargo-binstall
