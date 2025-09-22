1814c0d93ec5328d6b11a991a5e868f88db689b0
Remo Senekowitsch <remo@buenzli.dev>
2025-09-22 16:46:59 +0200

    add jj diff style to theme toggle

--- dot_local/bin2/executable_toggle_theme.fish
+++ dot_local/bin2/executable_toggle_theme.fish
@@ -11,6 +11,8 @@ if test "$(gsettings get org.gnome.desktop.interface color-scheme)" = "'prefer-d
     sd '# import' import ~/.config/alacritty/alacritty.toml
     sd 'theme ".*"' 'theme "iceberg-light"' ~/.config/zellij/config.kdl
     sd 'color_theme = ".*"' 'color_theme = "whiteout"' ~/.config/btop/btop.conf
+    sd '^"diff added token".*$' '"diff added token" = { bg = "#c0e0c0", fg = "#000000" }' ~/.config/jj/config.toml
+    sd '^"diff removed token".*$' '"diff removed token" = { bg = "#e0c0c0", fg = "#000000" }' ~/.config/jj/config.toml
 else
     gsettings set org.gnome.desktop.interface color-scheme prefer-dark
     gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3-dark
@@ -19,6 +21,8 @@ else
     sd '^import' '# import' ~/.config/alacritty/alacritty.toml
     sd 'theme ".*"' 'theme "kanagawa"' ~/.config/zellij/config.kdl
     sd 'color_theme = ".*"' 'color_theme = "Default"' ~/.config/btop/btop.conf
+    sd '^"diff added token".*$' '"diff added token" = { bg = "#204020", fg = "#bbddbb" }' ~/.config/jj/config.toml
+    sd '^"diff removed token".*$' '"diff removed token" = { bg = "#402020", fg = "#ddbbbb" }' ~/.config/jj/config.toml
 end
 
 killall -USR1 hx
